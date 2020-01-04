64837df1e74648b8561a2d87fd7df973a96454c7
costadouglas <costa.douglas@hcosta.com.br>
2020-01-04 19:11:02 -0300

    Adicionando salvar formulario no banco

--- conectar.php
+++ conectar.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connect = mysqli_connect('localhost', 'dougl179_glossma', 'dougl179_glossma#');
+    $connect = mysqli_connect('127.0.0.1', 'dougl179_glossma', 'dougl179_glossma#');
     $db = mysqli_select_db($connect, 'dougl179_glossmakeup');
 } catch (Exception $ex) {
     var_dump($ex);
