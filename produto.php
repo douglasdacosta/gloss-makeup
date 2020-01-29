ae57131f5cc155e86c908613fa0db738f2f47254
costadouglas <costa.douglas@hcosta.com.br>
2020-01-28 21:31:06 -0300

    ajuste na pagina de produto

--- produto.php
+++ produto.php
@@ -8,77 +8,82 @@ $descricao = $produtos[$id]['descricao'];
 ?>
 <div class="banner" id="home" style="background:url(img/banner.png) no-repeat;">    
     <section class="screen" id="produtos">
-        <div class="row">
-            <div class="col-sm-4">
-                <br><br><br><br><br><br>
-                <img src="img/produtos/<?php echo $img ?>" class="img-responsive img-rounded" alt="Responsive image" alt="<?php echo $titulo ?>"/>
-            </div>
-            <div class="col-sm-4">
-                <br>
-                 <div class="row">
-                        <div class="row-heading">
-                            <h3 <p style="color:#ffffff;" class="blogTitle"><?php echo $titulo ?></p></h3>
-                        </div>
-                    </div>
-                <img src="img/produtos/../produtos/banne.jpeg" class="img-responsive"/>
-            </div>
-            <div class="col-sm-4">
-                <div class="container">
-                    <div class="banner-caption">		
-                        <div class="col-md-5">
-                            <div class="row">   
-                                <div class="row-heading">
-                                    <h2>Cadastre-se <span>Aqui!</span></h2>
-                                </div>
-                                <div class="container">
-                                    <div class="col-md-5 col-sm-12">
-                                        <div class="row contact-form">
-                                            <form name="cadastroForm" id="cadastroForm" novalidate>
-                                                <div class="col-sm-10">
-                                                    <div class="form-group">
-                                                        <input type="text" class="form-control" placeholder="Seu nome *" id="name" required data-validation-required-message="Please enter your name.">
-                                                        <p class="help-block text-danger"></p>
-                                                    </div>
-                                                </div>    
-                                                <div class="col-sm-10">
-                                                    <div class="form-group">
-                                                        <input type="email" class="form-control" placeholder="Email *" id="email" required data-validation-required-message="Please enter your email address.">
-                                                        <p class="help-block text-danger"></p>
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-10">
-                                                    <div class="form-group">
-                                                        <input type="tel" class="form-control" placeholder="Telefone (WhatsApp)*" id="phone" required data-validation-required-message="Please enter your phone number.">
-                                                        <p class="help-block text-danger"></p>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-sm-10">
-                                                    <div class="form-group">
-                                                        <input type="text" class="form-control"  placeholder="Qual sua idade? *" id="subject">
-                                                        <p class="help-block text-danger"></p>
-                                                    </div>
-                                                </div>
-                                                <div class="col-xs-12">
-                                                    <div id="success"></div>
-                                                    <button type="submit" class="btn btn-success btn-caption btn-cadastro submit">Enviar Cadastro</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>	
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
             <div class="row">
-                <div class="row-heading">
-                    <h4> <p style="color:#ffffff;" ><?php echo $descricao ?></p></h4>
+                <div class="col-sm-12">
+                    <h2 class="title-produto"><?php echo $titulo ?></h2>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="texto-preco">
+                    <img src="img/produtos/<?php echo $img ?>" class="img-responsive img-rounded" alt="Responsive image" alt="<?php echo $titulo ?>"/>
+                </div>
+            </div>
+            <div class="col-sm-4" >
+                <div class="texto-preco">
+                    <h1> <p class="texto-preto">
+                            Preço Único R$10,00
+                        </p>
+                    </h1>
+
+                    <h6><p class="texto-preto">Praça Coronel Cardoso Franco 1A, Piratininga</p></h6>
+
+                    <h4><p class="texto-preto">@GLOSSMAKEYPBR</p></h4>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="row">   
+                    <div class="row-heading">
+                        <h2><div class="title-produto">Cadastre-se</div> <span>Aqui!</span></h2>
+                    </div>
+                    <div class="container">
+                        <div class="col-sm-5">
+                            <div class="row contact-form">
+                                <form name="cadastroForm" id="cadastroForm" action="cadastroEmail.php" method="POST" novalidate>
+                                    <div class="col-sm-10">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="Seu nome *" id="nome" name="nome" required data-validation-required-message="Please enter your name.">
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>    
+                                    <div class="col-sm-10">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" placeholder="Email *" id="email" name="email" required data-validation-required-message="Please enter your email address.">
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-10">
+                                        <div class="form-group">
+                                            <input type="tel" class="form-control" placeholder="Telefone (WhatsApp)*" id="telefone" name="telefone" required data-validation-required-message="Please enter your phone number.">
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-10">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control"  placeholder="Qual sua idade? *" name="idade" id="idade">
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-10">
+                                        <p class="title-produto">Ao clicar no botão "Cadastrar" eu declaro<br> que aceito as condições de <br>Termos de Uso e Política de Privacidade</p>
+                                        <div id="success"></div>
+                                        <button type="submit" class="btn btn-success btn-caption btn-cadastro submit">Enviar Cadastro</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>	
+                    </div>
                 </div>
             </div>
 
-    </section>
+        </div>
+    </section>    
+    <div class="row">
+        <div class="row-heading descricao-produto">
+            <h4> <p style="color:#ffffff;" ><?php echo $descricao ?></p></h4>
+        </div>
+    </div>
 </div>
 <?php
 include_once './rodape.php';
